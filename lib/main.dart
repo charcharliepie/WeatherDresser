5978a9737c575d1bbd695ea7fedb3fdd9879edae
Charlie Chiu <41720382+charcharliepie@users.noreply.github.com>
2019-09-27 21:07:49 +1000

    updated dependencies & built android

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(WeatherDresser());
 
-class MyApp extends StatelessWidget {
+class WeatherDresser extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Weather Dresser',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -18,7 +18,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        brightness: Brightness.light,
+        fontFamily: 'Poppins',
+
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
