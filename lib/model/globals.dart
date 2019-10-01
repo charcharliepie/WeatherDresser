6dadc76677995f0dc6cff3805571f1144202d1dd
Charlie Chiu <41720382+charcharliepie@users.noreply.github.com>
2019-10-01 11:16:02 +1000

    heroku url

--- lib/model/globals.dart
+++ lib/model/globals.dart
@@ -1,4 +1,4 @@
 library weather_dresser.globals;
 
 String microserviceBaseUrl = "http://127.0.0.1:3000"; // local microservice
-// String microserviceBaseUrl = "http://weatherdresser.herokuapp.com"; // testing heroku
+// String microserviceBaseUrl = "https://weatherdresser.herokuapp.com"; // testing heroku
